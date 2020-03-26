00e6c42520d88a8d76ff79a00ad5ed9c9924ca62
thiagoos16 <tos@icomp.ufam.edu.br>
2020-03-25 21:42:20 -0400

    add login view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+//import Header from './Header';
+import './global.css';
+import Logon from './pages/Logon';
 
 function App() {
+  // const [counter, setCounter] = useState(0);
+
+  // function increment() {
+  //   setCounter(counter + 1);
+  // }
+
+  // return (
+  //   <div>
+  //     <Header title="Be the batman"> Counter: {counter} </Header>
+  //     <button onClick={increment}> Increment </button>
+  //   </div>
+  // );
   return (
-    <h1> Hello Spawn </h1>
-  );
+    <div>
+      <Logon />
+    </div>
+  )  
 }
 
 export default App;
